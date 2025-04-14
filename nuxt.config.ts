e958abeefe96533ecf1195a970ec38b4a97b7bc5
rebosante <jcamayo@topdoctors.es>
2025-04-15 00:02:14 +0200

    resolved nuxt-i18n compatibility issues - moved locales to i18n folder - updated packages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -26,8 +26,8 @@ export default defineNuxtConfig({
     compilation: {
       strictMessage: false,
     },
-    lazy: false,
-    langDir: './locales',
+    lazy: true,
+    langDir: 'locales',
     strategy: 'no_prefix',
     locales: [
       { code: 'en-US', iso: 'en-US', name: 'English', file: 'en-US.json' },
